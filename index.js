6021967060296cc72e87d014df0ec2a9aeda3a28
thaemrangel <thaemrangel@gmail.com>
2021-03-05 19:46:06 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,14 @@
+import { Cliente } from "./Cliente.js";
+import { ContaPoupanca } from "./Conta/ContaPoupanca.js";
+import { ContaCorrente } from "./Conta/ContaCorrente.js";
+import { ContaSalario } from "./Conta/ContaSalario.js";
+
+const cliente1 = new Cliente("Ricardo", 11122233309);
+
+const contaCorrenteRicardo = new ContaCorrente(cliente1, 1001);
+const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
+const contaSalario = new ContaSalario(cliente1);
+contaSalario.depositar(100);
+contaSalario.sacar(10);
+
+console.log(contaSalario);
